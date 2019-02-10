e033a16225ec730b9f6475026ebde21c49768d98
sethkrantzler <sethkrantzler@gmail.com>
2019-02-10 12:22:17 -0800

    App Setup and Style Test

---
+++ public/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './styles/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <br></br>
+        <div id="holder">
+          <h2>OLYMPIA PIZZA</h2>
+          <p id="pcolor1">The opinions below are from 9 individuals who split one large and 3 medium pizzas. This pizza place allows you to do half and half pizzas with different flavors!! I mean who doesn't love Mod Pizza?? If you don't like creating your own pizza with any and every topping you ever wanted freshly made for one set price then something is wrong with you lmao! The only thing someone could complaint about is long lines and potentially service but I've been lucky for the most part to not have to really deal with that. If you're by a MoD Pizza you def should check it out! Every ingredient in the pizza was fresh, our waitress was kind & accommodating, and the vibes were great.</p>
+          <br></br>
+          <h1>OLYMPIA PIZZA</h1>
+          <p id="pcolor2">The opinions below are from 9 individuals who split one large and 3 medium pizzas. This pizza place allows you to do half and half pizzas with different flavors!! I mean who doesn't love Mod Pizza?? If you don't like creating your own pizza with any and every topping you ever wanted freshly made for one set price then something is wrong with you lmao! The only thing someone could complaint about is long lines and potentially service but I've been lucky for the most part to not have to really deal with that. If you're by a MoD Pizza you def should check it out! Every ingredient in the pizza was fresh, our waitress was kind & accommodating, and the vibes were great.</p>
+          <h3>OLYMPIA PIZZA</h3>
+          <p id="pcolor1">The opinions below are from 9 individuals who split one large and 3 medium pizzas. This pizza place allows you to do half and half pizzas with different flavors!! I mean who doesn't love Mod Pizza?? If you don't like creating your own pizza with any and every topping you ever wanted freshly made for one set price then something is wrong with you lmao! The only thing someone could complaint about is long lines and potentially service but I've been lucky for the most part to not have to really deal with that. If you're by a MoD Pizza you def should check it out! Every ingredient in the pizza was fresh, our waitress was kind & accommodating, and the vibes were great.</p>
+          <br></br>
+          <h4>OLYMPIA PIZZA</h4>
+          <p id="pcolor2">The opinions below are from 9 individuals who split one large and 3 medium pizzas. This pizza place allows you to do half and half pizzas with different flavors!! I mean who doesn't love Mod Pizza?? If you don't like creating your own pizza with any and every topping you ever wanted freshly made for one set price then something is wrong with you lmao! The only thing someone could complaint about is long lines and potentially service but I've been lucky for the most part to not have to really deal with that. If you're by a MoD Pizza you def should check it out! Every ingredient in the pizza was fresh, our waitress was kind & accommodating, and the vibes were great.</p>
+          <h1>OLYMPIA PIZZA</h1>
+          <p id="pcolor1">The opinions below are from 9 individuals who split one large and 3 medium pizzas. This pizza place allows you to do half and half pizzas with different flavors!! I mean who doesn't love Mod Pizza?? If you don't like creating your own pizza with any and every topping you ever wanted freshly made for one set price then something is wrong with you lmao! The only thing someone could complaint about is long lines and potentially service but I've been lucky for the most part to not have to really deal with that. If you're by a MoD Pizza you def should check it out! Every ingredient in the pizza was fresh, our waitress was kind & accommodating, and the vibes were great.</p>
+          <br></br>
+          <h2>OLYMPIA PIZZA</h2>
+          <p id="pcolor2">The opinions below are from 9 individuals who split one large and 3 medium pizzas. This pizza place allows you to do half and half pizzas with different flavors!! I mean who doesn't love Mod Pizza?? If you don't like creating your own pizza with any and every topping you ever wanted freshly made for one set price then something is wrong with you lmao! The only thing someone could complaint about is long lines and potentially service but I've been lucky for the most part to not have to really deal with that. If you're by a MoD Pizza you def should check it out! Every ingredient in the pizza was fresh, our waitress was kind & accommodating, and the vibes were great.</p>
+          <h4>OLYMPIA PIZZA</h4>
+          <p id="pcolor1">The opinions below are from 9 individuals who split one large and 3 medium pizzas. This pizza place allows you to do half and half pizzas with different flavors!! I mean who doesn't love Mod Pizza?? If you don't like creating your own pizza with any and every topping you ever wanted freshly made for one set price then something is wrong with you lmao! The only thing someone could complaint about is long lines and potentially service but I've been lucky for the most part to not have to really deal with that. If you're by a MoD Pizza you def should check it out! Every ingredient in the pizza was fresh, our waitress was kind & accommodating, and the vibes were great.</p>
+          <br></br>
+          <h3>OLYMPIA PIZZA</h3>
+          <p id="pcolor2">The opinions below are from 9 individuals who split one large and 3 medium pizzas. This pizza place allows you to do half and half pizzas with different flavors!! I mean who doesn't love Mod Pizza?? If you don't like creating your own pizza with any and every topping you ever wanted freshly made for one set price then something is wrong with you lmao! The only thing someone could complaint about is long lines and potentially service but I've been lucky for the most part to not have to really deal with that. If you're by a MoD Pizza you def should check it out! Every ingredient in the pizza was fresh, our waitress was kind & accommodating, and the vibes were great.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
