b922e083db751164fdf587ce939321cc80a7af22
sethkrantzler <sethkrantzler@gmail.com>
2019-03-09 18:51:35 -0800

    Rating objects added

--- SethLovesReact/src/components/ReviewCardRatingSection/ReviewCardRatingSection.tsx
+++ SethLovesReact/src/components/ReviewCardRatingSection/ReviewCardRatingSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ReviewCardRatingSectionStyles.scss';
 
 export interface IRatingSectionProps {
   priceVal: number;
@@ -15,12 +16,23 @@ export class ReviewCardRatingSection extends React.Component<IRatingSectionProps
 
   public render() {
     return (
-      <div>
-        <p>{this.props.priceVal}/5</p>
-        <p>{this.props.tasteVal}/5</p>
-        {this.props.decorVal   ? <p>{this.props.decorVal}/5</p> : null}
-        {this.props.boxVal     ? <p>{this.props.boxVal}/5</p> : null}
-        {this.props.overallVal     ? <p>{this.props.overallVal}/5</p> : null}
+      <div className="rating-holder">
+        <div className="rating-object">
+          <p>Price: {this.props.priceVal}/5</p>
+        </div>
+        <div className="rating-object">
+          <p>Taste: {this.props.tasteVal}/5</p>
+        </div>
+        {this.props.decorVal   ? <div className="rating-object">
+          <p>Decor: {this.props.decorVal}/5</p>
+        </div> : null}
+        {this.props.boxVal     ? <div className="rating-object">
+          <p>Box: {this.props.boxVal}/5</p>
+        </div> : null} 
+        <div className="rating-object">
+          <p>Overall: {this.props.overallVal}/5</p>
+        </div>
+
       </div>
     );
   }
---
+++ SethLovesReact/src/reviews/IansReview.tsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export default class IansReview extends React.Component<any, any> {
+    constructor(props:any) {
+        super(props);
+    }
+
+    render () {
+        return (
+            <div>
+                <p>Ian's Pizza is a Seattle classic. Located right on Broadway in Capitol Hill, it's a hop skip 
+                    and a jump from some of the best parts of Seattle. If you're in the mood for a slice out with your friends, 
+                    Ian's is a great spot for a stop. I just so happened to be out by Ian's picking up some art supplies 
+                    at Blick next door, and decided to stop in for a few slices with my girlfriend Devon to kick off 
+                    SLP. I asked the cashier to run me through the pies they had. I wasn't sure what to get as she 
+                    ran through a trove of different pies: a philly cheesesteak, buffalo chicken, vegan burger, 
+                    tomatoe pesto, mac and cheese. We ended up taking 4 differently slices based off some of her 
+                    recommendations and some of our favorites.</p>
+                    <p>
+                        </p><&nbsp>Mac and cheese: Who doesn't love mac and cheese? Yeah it might not be what you always want on your pizza, but if someone offered you a bite you know you'd try it with no hesitation. It's pretty plain overall and benefits from some accesories like honey and chili flake.</p>
+            </div>
+                
+
+        )
+    }
+}
